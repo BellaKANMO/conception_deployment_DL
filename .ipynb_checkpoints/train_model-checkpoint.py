584f41245f80a034d2f919884acfce2327ce6078
Bella KANMO <bellarindakn@gmail.com>
2025-11-04 10:56:54 +0100

    modification of the train_model.py file

---
+++ .ipynb_checkpoints/train_model-checkpoint.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import mlflow
+import mlflow.tensorflow
+
+#Variables pour les parametres
+EPOCHS = 5
+BATCH_SIZE = 128
+DROPOUT_RATE = 0.2
+L2_FACTOR = 0.001
+
+#Lancement de la session de suivi MLflow
+with mlflow.start_run():
+     # Enregistrement des param tres
+    mlflow.log_param("epochs", EPOCHS)
+    mlflow.log_param("batch_size", BATCH_SIZE)
+    mlflow.log_param("dropout_rate", DROPOUT_RATE)
+    mlflow.log_param("l2_factor", L2_FACTOR)
+
+# Construction et entra nement du modele (utiliser les variables definies)
+
+# chargement des  donnees mnist
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+#Use 90% for training and 10% for validation (dev)
+x_val = x_train[54000:]
+y_val = y_train[54000:]
+x_train = x_train[:54000]
+y_train = y_train[:54000]
+# normalisation des donnees
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+
+#redimmensionnnement des images pour le reseaux fully connected
+x_train = x_train.reshape((60000, 784))
+x_test = x_test.reshape((10000, 784))
+
+# construction du modele
+model = keras.Sequential(
+    [
+        keras.layers.Dense(512, activation='relu', input_shape=(784,),  kernel_regularizer=keras.regularizers.l2(L2_FACTOR)),
+        keras.layers.Dropout(DROPOUT_RATE),
+        keras.layers.Dense(10, activation='softmax')
+    ]
+)
+
+history = model.fit(
+x_train,
+y_train,
+epochs=5,
+batch_size=128,
+validation_data=(x_val, y_val) # Use the validation set
+)
+
+
+#Compilation du modele
+model.compile(
+    optimizer="adam",
+    loss = "sparse_categorical_crossentropy",
+    metrics = ["accuracy"]
+)
+
+#Entrainement du modele
+history = model.fit(
+    x_train,
+    y_train,
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
+    validation_split = 0.1
+)
+
+#Evaluation du modele
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f"Précision sur les donn es de test: {test_acc:.4f}")
+
+ # Enregistrement des metriques
+mlflow.log_metric("test_accuracy", test_acc)
+mlflow.log_metric("test_loss", test_loss)
+
+ # Enregistrement du mod le complet
+mlflow.keras.log_model(model, "mnist-model-regularized")
+print(" Modele sauvegardé sous mnist_model_regularized dans MLflow")
+
+#Sauvegarde du modele
+#model.save("mnist_model.h5")
+#print(" Mod le sauvegard sous mnist_model.h5")
+
+
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['loss'], label='train_loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.title('Loss - Regularized Model')
+plt.legend()
+plt.show()
+
+plt.plot(history.history['accuracy'], label='train_acc')
+plt.plot(history.history['val_accuracy'], label='val_acc')
+plt.title('Accuracy - Regularized Model')
+plt.legend()
+plt.show()
--- train_model.py
+++ train_model.py
@@ -8,6 +8,7 @@ import mlflow.tensorflow
 EPOCHS = 5
 BATCH_SIZE = 128
 DROPOUT_RATE = 0.2
+L2_FACTOR = 0.001
 
 #Lancement de la session de suivi MLflow
 with mlflow.start_run():
@@ -15,56 +16,99 @@ with mlflow.start_run():
     mlflow.log_param("epochs", EPOCHS)
     mlflow.log_param("batch_size", BATCH_SIZE)
     mlflow.log_param("dropout_rate", DROPOUT_RATE)
+    mlflow.log_param("l2_factor", L2_FACTOR)
 
 # Construction et entra nement du modele (utiliser les variables definies)
 
 # chargement des  donnees mnist
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
+#Use 90% for training and 10% for validation (dev)
+x_val = x_train[54000:]
+y_val = y_train[54000:]
+x_train = x_train[:54000]
+y_train = y_train[:54000]
 # normalisation des donnees
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
 
 #redimmensionnnement des images pour le reseaux fully connected
-x_train = x_train.reshape((60000, 784))
+x_train = x_train.reshape((54000, 784))
+x_val = x_val.reshape((6000, 784))
 x_test = x_test.reshape((10000, 784))
 
-# construction du modele
-model = keras.Sequential(
-    [
-        keras.layers.Dense(512, activation='relu', input_shape=(784,)),
-        keras.layers.Dropout(0.2),
-        keras.layers.Dense(10, activation='softmax')
-    ]
-)
+optimizers = {
+    "SGD_with_momentum": keras.optimizers.SGD(learning_rate=0.01, momentum=0.9),
+    "RMSprop": "rmsprop",
+    "Adam": "adam"
+}
+for opt_name, optimizer in optimizers.items():
+    with mlflow.start_run(run_name=f"Optimizer_Comparison_{opt_name}"):
+        # construction du modele
+        model = keras.Sequential(
+            [
+                keras.layers.Dense(512, activation='relu', input_shape=(784,),  kernel_regularizer=keras.regularizers.l2(L2_FACTOR)),
+                keras.layers.Dropout(DROPOUT_RATE),
+                keras.layers.Dense(10, activation='softmax')
+            ]
+        )
 
-#Compilation du modele
-model.compile(
-    optimizer="adam",
-    loss = "sparse_categorical_crossentropy",
-    metrics = ["accuracy"]
-)
 
-#Entrainement du modele
-history = model.fit(
-    x_train,
-    y_train,
-    epochs=5,
-    batch_size = 128,
-    validation_split = 0.1
-)
+        #Compilation du modele
+        model.compile(
+            optimizer=optimizer,
+            loss = "sparse_categorical_crossentropy",
+            metrics = ["accuracy"]
+        )
 
-#Evaluation du modele
-test_loss, test_acc = model.evaluate(x_test, y_test)
-print(f"Pr cision sur les donn es de test: {test_acc:.4f}")
+        history = model.fit(
+        x_train,
+        y_train,
+        epochs=5,
+        batch_size=128,
+        validation_data=(x_val, y_val) # Use the validation set
+        )
 
- # Enregistrement des metriques
-mlflow.log_metric("test_accuracy", test_acc)
 
- # Enregistrement du mod le complet
-mlflow.keras.log_model(model, "mnist-model")
-print(" Modele sauvegardé sous mnist_model")
+
+
+        #Entrainement du modele
+        history = model.fit(
+            x_train,
+            y_train,
+            epochs=EPOCHS,
+            batch_size=BATCH_SIZE,
+            validation_split = 0.1
+        )
+
+        #Evaluation du modele
+        test_loss, test_acc = model.evaluate(x_test, y_test)
+        print(f"Précision sur les donn es de test: {test_acc:.4f}")
+
+        # Log metrics and parameters with MLflow
+        mlflow.log_param("optimizer", opt_name)
+        mlflow.log_metric("test_accuracy", test_acc)
+        mlflow.log_metric("test_loss", test_loss)
+
+# Enregistrement du mod le complet
+mlflow.keras.log_model(model, name = "mnist-model-regularized")
+print(" Modele sauvegardé sous mnist_model_regularized dans MLflow")
 
 #Sauvegarde du modele
 #model.save("mnist_model.h5")
 #print(" Mod le sauvegard sous mnist_model.h5")
+
+
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['loss'], label='train_loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.title('Loss - Regularized Model')
+plt.legend()
+plt.show()
+
+plt.plot(history.history['accuracy'], label='train_acc')
+plt.plot(history.history['val_accuracy'], label='val_acc')
+plt.title('Accuracy - Regularized Model')
+plt.legend()
+plt.show()
